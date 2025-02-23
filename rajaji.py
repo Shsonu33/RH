6dd57239832d2a936f101316d7d20f613fbc2e42
Shsonu33 <189646112+Shsonu33@users.noreply.github.com>
2025-02-23 12:29:56 +0530

    Add files via upload

---
+++ rajaji.py
@@ -0,0 +1,17 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7313487631:AAGpOo4UcgVp01I49LvH40iAuP6et1_1hKw")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@Itz_sonu_9")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6005957043")
+
+
+
+
+
+  
